dacbafd55d4d1d942b53638a0712ec3c1b127dee
GabrielaEvtimova <147919001+GabrielaEvtimova@users.noreply.github.com>
2024-06-29 18:03:12 +0300

    Refactoring - add own module for replaceTemplate function

--- Section-1/node-farm-1/starter/index.js
+++ Section-1/node-farm-1/starter/index.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const replaceTemplate = require("./modules/replaceTemplate");
+
 
 //////////////////////////////////////////////////
 // FILES
@@ -40,21 +42,7 @@ const url = require("url");
 
 // Read the data only once before executing the server
 
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%ID%}/g, product.id);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
 
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/, "not-organic");
-
-  return output;
-};
 
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
