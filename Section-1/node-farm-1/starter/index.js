4fa57ce2632e3c49e2280540489198d49c390166
GabrielaEvtimova <147919001+GabrielaEvtimova@users.noreply.github.com>
2024-06-29 10:00:44 +0300

    Build a (very) simple api

--- Section-1/node-farm-1/starter/index.js
+++ Section-1/node-farm-1/starter/index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const http = require("http");
+const url = require("url");
 
 //////////////////////////////////////////////////
 // FILES
@@ -36,6 +37,11 @@ const http = require("http");
 
 //////////////////////////////////////////////////
 // SERVER
+
+// Read the data only once before executing the server
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObject = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
@@ -43,6 +49,12 @@ const server = http.createServer((req, res) => {
     res.end("This is the OVERVIEW!");
   } else if (pathName === "/product") {
     res.end("This is the PRODUCT!");
+  } else if (pathName === "/api") {
+    res.writeHead(200, {
+      "Content-type": "application/json",
+    });
+
+    res.end(data);
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
